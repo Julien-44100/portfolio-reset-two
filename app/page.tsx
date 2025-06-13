20a1056eddc89a6fb87d135f8e5e5df669360999
Julien-44100 <julienbotar44100@yahoo.com>
2025-06-13 21:11:05 +0200

    final test vercel new branch

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<div className={styles.page}>
+			<h1>bonjour</h1>
 			{/* <h1 className={styles.slideInFromLeft}>
 				Je suis Julien Botar, développeur web junior, découvrez mon
 				savoir-faire à travers ce portfolio.
